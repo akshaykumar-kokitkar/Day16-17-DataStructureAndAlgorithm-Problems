90e0bd97a85d5007b914af37ecc4cec1b3be3eb9
ak47 <kokitkarak@gmail.com>
2022-01-08 23:08:05 +0530

    Data structure and algorith problems

---
+++ AlgorithProblems/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AlgorithProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Algorithm Programs");
+            UserInput userInput = new UserInput();
+        }
+    }
+}
